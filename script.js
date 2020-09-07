a3838bb30b264e40927bce18ec4f381509a52c6c
Vladimir <vminenko@ukr.net>
2020-09-07 20:14:32 +0300

    Add uslovia and ternarnyi operator

--- script.js
+++ script.js
@@ -21,22 +21,62 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
-    genres: [],
+    genres: [], 
     privat: false
 };
 
-const a = prompt ('Один из последних просмотренных фильмов?',''),
-      b = prompt ('На сколько оцените его?',''),
-      c = prompt ('Один из последних просмотренных фильмов?',''),
-      d = prompt ('На сколько оцените его?','');
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
 
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-      console.log(personalMovieDB);
+console.log(personalMovieDB);
+
+//! Условия
+
+if (4 == 9) {
+    console.log('Ok!');
+} else {
+    console.log(Error);
+}
+
+const num = 50;
+
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Много');
+} else {
+    console.log('Ok!');
+}
+
+//!Тернарный оператор
+const num = 50;
+
+(num === 50) ? console.log('Ok!'): console.log('Error');
+
+const num = 50
+
+switch (num) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('В точку');
+        break;
+    default:
+        console.log('НЕ в этот раз');
+        break;
+}
